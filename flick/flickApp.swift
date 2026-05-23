2b5c5323f3e1d028ab2fe0986592d58c435a343c
nandan-varma <nandanvarma.me@gmail.com>
2026-05-23 00:28:12 -0700

    Initial Commit

---
+++ flick/flickApp.swift
@@ -0,0 +1,17 @@
+//
+//  flickApp.swift
+//  flick
+//
+//  Created by Nandan Varma on 5/23/26.
+//
+
+import SwiftUI
+
+@main
+struct flickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
